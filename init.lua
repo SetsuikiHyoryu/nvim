616dffdbc7e014cb284a8167c374244e24b5cc5d
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2024-10-17 18:55:23 +0900

    chore: update init config contstruct
    - 修改 leader 键为空格。
    - 增加 `have_nerd_font` 全局属性（貌似不是原生属性）。
    - 拆分原生配置项至新文件并提升调用位置。

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Neovim 原生即有的配置项。
+require 'custom.native-options'
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
