3db7011398fab7647bc1ba55e30949c320431013
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2026-03-31 13:48:08 +0900

    chore(LSP): remove big `maxTsServerMemory`

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -2,7 +2,7 @@
 local vue_lsp_location = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
 local vue_plugin = {
   name = '@vue/typescript-plugin',
-  -- 如果项目路径下的 `node_modules` 中安装了 `@vue/typescript-plugin`，则 `location` 可以为任意值
+  -- 如果项目路径下的 `node_modules` 中安装了 `@vue/typescript-plugin`，则 `location` 可以为任意值。
   location = vue_lsp_location,
   languages = { 'vue' },
   configNamespace = 'typescript',
@@ -12,7 +12,7 @@ return {
   init_options = {
     -- 一些大型 TS 项目需要分配较多内存以避免 ts_ls 崩溃。
     -- 若非开发这些项目，最好将这里注释。
-    maxTsServerMemory = 8192,
+    -- maxTsServerMemory = 8192,
 
     plugins = {
       vue_plugin,
