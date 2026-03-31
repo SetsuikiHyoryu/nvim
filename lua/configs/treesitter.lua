40ebac77bcd19577fd0440603d0fae4235d74b62
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2026-04-01 02:28:20 +0900

    chore(treesitter): remove outdated master branch config

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -4,13 +4,3 @@ vim.pack.add {
     version = 'main',
   },
 }
-
--- **Release 0.12 后，`nvim-treesitter` 不需要再手动调用 `setup` 了，**
--- **但不清楚 Windows 下这句是否还要。**
--- **暂时将此句注释。**
---
--- 增加这个配置是因为下载 git 相关的解析器时发生了错误。
--- 根据 `Windows support#How will the parser be downloaded?` 章节的说法，
--- 通过 `git` 下载解析器的行为将被弃用，推荐 Windows 下也使用 `curl` 的方式。
--- See <https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#which-c-compiler-will-be-used>
--- require('nvim-treesitter.install').prefer_git = false
