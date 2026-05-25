64a40dba872792da092c128125a785b09e1fbb78
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2026-05-25 23:29:27 +0900

    feat(autoformat): add format on save toggle method

--- lua/configs/autoformat.lua
+++ lua/configs/autoformat.lua
@@ -3,11 +3,17 @@ vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
 require('conform').setup {
   notify_on_error = false,
 
-  -- 开发者说是推荐。
-  format_on_save = {
-    lsp_format = 'fallback',
-    timeout_ms = 500,
-  },
+  format_on_save = function()
+    if vim.g.disable_autoformat then
+      return
+    end
+
+    -- 开发者说是推荐这组属性值。
+    return {
+      lsp_format = 'fallback',
+      timeout_ms = 500,
+    }
+  end,
 
   -- See: https://github.com/stevearc/conform.nvim?tab=readme-ov-file#customizing-formatters
   -- See: :help conform-options
@@ -39,3 +45,12 @@ require('conform').setup {
 
 -- 似乎是插入模式下格式不符合预期时自动格式化。
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+vim.api.nvim_create_user_command('FormatToggle', function()
+  vim.g.disable_autoformat = not vim.g.disable_autoformat
+  if vim.g.disable_autoformat then
+    print 'format_on_save: OFF'
+  else
+    print 'format_on_save: ON'
+  end
+end, {})
