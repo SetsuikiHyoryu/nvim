08dde091d40c07836f72c46c91034aa25fd681af
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2026-06-20 03:17:23 +0900

    chore(icon): replace nvim-web-devicons with MiniIcons.mock_nvim_web_devicons

--- lua/configs/icon.lua
+++ lua/configs/icon.lua
@@ -1,3 +1,3 @@
 vim.pack.add { 'https://github.com/nvim-mini/mini.icons' }
 require('mini.icons').setup()
-vim.pack.add { 'https://github.com/nvim-tree/nvim-web-devicons' }
+MiniIcons.mock_nvim_web_devicons()
